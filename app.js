af55abbc8024c19915c3f280d04644724eba13ac
Julia <jujaspers@gmail.com>
2017-11-07 10:35:15 -0500

    second

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const app = express(); // creates an instance of an express application
 const morgan = require('morgan')
 
 
+app.use(morgan('dev'));
+
 app.listen(3000, function(){
 	console.log('server running');
 });
 
-app.use(morgan());
+app.use('/special', function(req, res, next){
+	console.log('this is special');
+	next();
+
+});
 
 app.get('/', function(req, res, next) {
 	console.log(req.path, req.method, res.statusCode); //extra credit!
