bca780e323d1add5a89975796049a7fc39ed2c61
Julia <jujaspers@gmail.com>
2017-11-07 12:03:16 -0500

    nunjucks

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
-const morgan = require('morgan')
+const morgan = require('morgan');
+const nunjucks=require('nunjucks');
 
+
+
+var locals = {
+    title: 'twitterjs',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+
+app.set('view engine', 'html');
+app.engine('html',nunjucks.render);
+nunjucks.configure('views', { noCache: true});
+
+// app.get('/', function(req, res, next) {
+// 	console.log(req.path, req.method, res.statusCode); //extra credit!
+// 	res.send('yay');
+// });
+
+app.get('/', function(req, res) {
+    const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+	return res.render('index', {title: 'Hall of Fame', people: people})
+});
+
+//-----------------------------------------------
 
 app.use(morgan('dev'));
 
@@ -13,11 +41,6 @@ app.use('/special', function(req, res, next){
 	console.log('this is special');
 	next();
 
-});
-
-app.get('/', function(req, res, next) {
-	console.log(req.path, req.method, res.statusCode); //extra credit!
-	res.send('yay');
 });
 
 app.get('/news', function(req, res, next) {
