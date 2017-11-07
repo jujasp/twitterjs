2f7483ade08dcbc58c11732ad0d1eda10ec5d4ee
Julia <jujaspers@gmail.com>
2017-11-07 15:08:23 -0500

    front page working!

--- app.js
+++ app.js
@@ -2,36 +2,29 @@ const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 const morgan = require('morgan');
 const nunjucks=require('nunjucks');
+const routes = require('./routes');
 
-
-app.set('view engine', 'html');
-app.engine('html',nunjucks.render);
-nunjucks.configure('views', { noCache: true});
+app.set('view engine', 'html'); //gives the extension for the engine
+app.engine('html',nunjucks.render);// gives the extension the file to be rendered.
+nunjucks.configure('views', { noCache: true});// set the psth for t
+app.use(morgan('dev'));
+app.use('/', routes);
 
 // app.get('/', function(req, res, next) {
 // 	console.log(req.path, req.method, res.statusCode); //extra credit!
 // 	res.send('yay');
 // });
 
-app.get('/', function(req, res) {
-    const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-	return res.render('index', {title: 'twitter.js', people: people})
-});
-
 //-----------------------------------------------
 
-app.use(morgan('dev'));
+
 
 app.listen(3000, function(){
 	console.log('server running');
 });
 
-app.use('/special', function(req, res, next){
-	console.log('this is special');
-	next();
+// app.use('/special', function(req, res, next){
+// 	console.log('this is special');
+// 	next();
 
-});
-
-app.get('/news', function(req, res, next) {
-	res.send('news');
-});
+// });
